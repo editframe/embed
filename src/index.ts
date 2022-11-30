799c8f9b765221ad268a03cdc63e742d262c494c
Nick Camillo <nick.camillo@gmail.com>
2022-11-30 00:47:56 -0500

    add setConfig method

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,42 @@
 class Embed {
-  private frame!: HTMLIFrameElement
+  private frame!: HTMLIFrameElement;
 
   /**
    * Constructs a new instance of an Editframe embed.
    * @param configuration options
    */
   constructor(id: string) {
-    const iframes = document.querySelectorAll('iframe')
-    const frame = Array.from(iframes).find((iframe) => iframe.id === id) || iframes[0]
-    if(!frame) return
-    this.frame = frame
+    const iframes = document.querySelectorAll("iframe");
+    const frame =
+      Array.from(iframes).find((iframe) => iframe.id === id) || iframes[0];
+    if (!frame) return;
+    this.frame = frame;
+  }
+
+  public async setConfig(config: any): Promise<boolean> {
+    return await this.sendCallWithValue("setConfig", config);
   }
 
   public async setTimeline(layers: any): Promise<boolean> {
-    return await this.sendCallWithValue('setTimeline', layers)
+    return await this.sendCallWithValue("setTimeline", layers);
   }
 
   private async sendCallWithValue(call: string, value: any): Promise<boolean> {
-    if(!this.frame.contentWindow) return false
+    if (!this.frame.contentWindow) return false;
     try {
-      this.frame.contentWindow.postMessage({
-        call,
-        value
-      }, '*')
-      return true
+      this.frame.contentWindow.postMessage(
+        {
+          call,
+          value,
+        },
+        "*"
+      );
+      return true;
     } catch (err) {
-      throw err
+      throw err;
     }
   }
 }
 
-export default Embed
+export default Embed;
+
